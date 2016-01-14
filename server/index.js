a1ecc55fc7df4c5cffdccee194db809ebfdca430
Mike Morisco <mikemorisco@gmail.com>
2016-01-14 13:33:50 -0500

    Revertting

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ var path = require('path'),
 
 app.use(express.static(path.join(__dirname, '../')));
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000! do it');
 });
