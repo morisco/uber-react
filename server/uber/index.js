d1fe1ffda5f54d4768428fa9ccdfd10438610784
Mike Morisco <mikemorisco@gmail.com>
2016-01-14 10:49:17 -0500

    Readding Keys

--- server/uber/index.js
+++ server/uber/index.js
@@ -1,11 +1,11 @@
 var Uber = require('node-uber');
 
 var uber = new Uber({
-  client_id: '*******',
-  client_secret: '*******',
-  server_token: '*******',
-  redirect_uri: '*******',
-  name: '*******',
+  client_id: 'MLtQTh5LrxlFRb6-R-i8ck7y6bsODfnT',
+  client_secret: 'dINPqb9Xy-y86bRDYnsikxYLmWC7UVcw4UE72GWU',
+  server_token: '5jKc-uxfLRGpa27CWxucHfZTxG4Cue6cjZNShhVh',
+  redirect_uri: 'https://ubermebro.com',
+  name: 'Do_It_Bro!'
 });
 
 module.exports = {
